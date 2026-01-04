b1681657276164a904d683724095c6a25bc26553
Plínio Peixoto Dos Santos <pliniobr33@gmail.com>
2026-01-04 16:11:46 -0300

    ltera server

--- src/server.js
+++ src/server.js
@@ -20,5 +20,5 @@ server.use(router); //rotas na prota 3000
 
 server.listen(8000, () => {
   console.log("Boas-Vindas a API do allbooks");
-  console.log("API disponível atraves da url http://localhost:8000");
+  console.log("API disponível em http://localhost:8000");
 });
